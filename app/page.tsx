48816d5c5b782760ecad787ce91bdaacc7ce61f1
Eric Zou <ezou626@gmail.com>
2023-09-09 13:26:55 -0400

    add next files and changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-
+import React from 'react';
 
 function HomePage() {
-
+    return (
+        <>
+            <h1 className='text-6xl py-20 font-bold text-blue-950'>ShareSpot</h1>
+            <h2 className='text-4xl py-10 text-slate-600'>The USB-based Social Media Platform</h2>
+        </>
+    )
 }
 
 export default HomePage;
