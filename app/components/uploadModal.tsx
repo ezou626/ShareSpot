98e706b818e4afa39266a005528369cd93ae8b91
Eric Zou <ezou626@gmail.com>
2023-09-10 06:23:02 -0400

    profile

--- app/components/uploadModal.tsx
+++ app/components/uploadModal.tsx
@@ -2,7 +2,7 @@ function UploadModal({unShowModal}: {unShowModal: () => void}) {
 
 return (
     <div className="fixed m-auto inset-0 flex flex-col justify-center bg-gray-800 z-50 space-y-5 w-60 h-60 p-5 rounded">
-        <button onClick={unShowModal}>Upload</button>
+        <button onClick={unShowModal} className="p-2 bg-red-600 hover:bg-red-400">Upload</button>
         <form>
           <h1>File Upload</h1>
           <input type="file" />
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,15 @@
-export default function() {
-    return <></>;
+"use client";
+import { useState } from "react";
+import NavigationBar from "../components/navigationBar";
+import UploadModal from "../components/uploadModal";
+
+export default function Profile() {
+
+    const [showModal, setShowModal] = useState(false);
+
+    return <>
+        <NavigationBar ></NavigationBar>
+        {showModal ? <UploadModal unShowModal={() => setShowModal(false)} /> : <></>}
+        <button>Add USB</button>
+    </>
 }
