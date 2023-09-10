bd3845d1069d771950e538427a667c50f4aa38c3
Eric Zou <ezou626@gmail.com>
2023-09-10 02:41:57 -0400

    try my code

--- app/components/loginModal.tsx
+++ app/components/loginModal.tsx
@@ -5,18 +5,18 @@ function Login({login, unShowModal}: {login: (username: string, password: string
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    function handleUsernameChange(input: { target: { name: SetStateAction<string>; }; }) {
-        setUsername(input.target.name);
+    function handleUsernameChange(input: { target: { value: SetStateAction<string>; }; }) {
+        setUsername(input.target.value);
     }
 
-    function handlePasswordChange(input: { target: { name: SetStateAction<string>; }; }) {
-        setPassword(input.target.name);
+    function handlePasswordChange(input: { target: { value: SetStateAction<string>; }; }) {
+        setPassword(input.target.value);
     }
 
     return <div className='fixed m-auto inset-0 flex flex-col justify-center bg-gray-800 z-50 space-y-5 w-60 h-60 p-5 rounded'>
         <button onClick={unShowModal} className='rounded bg-red-600 hover:bg-red-400 text-white float-right px-1'>Close</button>
-        <input className='rounded border-gray-500 bg-inherit pt-5 text-white' placeholder="Username" onChange={handleUsernameChange} />
-        <input className='rounded border-gray-500 bg-inherit pb-5 text-white' type="password" placeholder="Password" onChange={handlePasswordChange}/>
+        <input className='rounded border-gray-500 bg-inherit pt-5 text-white' placeholder="Username" value={username} onChange={handleUsernameChange} id='username' />
+        <input className='rounded border-gray-500 bg-inherit pb-5 text-white' type="password" placeholder="Password" value={password} onChange={handlePasswordChange} id='password'/>
         <button onClick={() => {login(username, password)}} className='rounded bg-blue-600 hover:bg-blue-400 text-white float-left px-1'>Submit</button>
     </div>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -25,15 +25,14 @@ function HomePage() {
         if (user.email != "") {
             return;
         }
-        fetch('https://careful-granite-398516.ue.r.appspot.com:5000/login', {
+        console.log("hello")
+        fetch('http://10.102.91.87:5000/login', {
             method: 'POST',
             headers: {
-                'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({"user email": username, "user password": password})
         })
-        .then(response => response.json())
         .then(response => {
             console.log(response);
             if (user.email != "") {
@@ -45,7 +44,7 @@ function HomePage() {
     return (
         <>
             <NavigationBar showModal={() => {setShowModalWrapper(true)}} />
-            {showModal ? <Login login={() => loginWrapper} unShowModal={() => setShowModalWrapper(false)}/> : <></>}
+            {showModal ? <Login login={loginWrapper} unShowModal={() => setShowModalWrapper(false)}/> : <></>}
              <main className='px-10 overflow-y-clip space-y-5 py-32 mx-auto max-w-5xl animate-revealbody'>
                 <h1 className='text-6xl font-bold text-blue-950'>ShareSpot</h1>
                 <h2 className='text-4xl text-slate-600'>The USB-based Social Media Platform</h2>
