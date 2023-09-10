47dea2600fe925e9a313e11ff05604643474d034
Eric Zou <ezou626@gmail.com>
2023-09-10 03:48:17 -0400

    add error

--- app/components/loginModal.tsx
+++ app/components/loginModal.tsx
@@ -17,7 +17,7 @@ function Login({login, unShowModal, error}: {login: (username: string, password:
         <button onClick={unShowModal} className='rounded bg-red-600 hover:bg-red-400 text-white float-right px-1'>Close</button>
         <input className='rounded border-gray-500 bg-inherit pt-5 text-white' placeholder="Username" onChange={handleUsernameChange} />
         <input className='rounded border-gray-500 bg-inherit pb-5 text-white' type="password" placeholder="Password" onChange={handlePasswordChange}/>
-        <p hidden></p>
+        <p hidden={error==''}>{error}</p>
         <button onClick={() => {login(username, password);       
         }} className='rounded bg-blue-600 hover:bg-blue-400 text-white float-left px-1'>Submit</button>
     </div>
